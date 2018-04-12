6fb5bcf62f70367fdcc766b6a421e7c2f317f551
Shan Qamruddin <Shanaliq@gmail.com>
2018-04-12 11:31:51 -0400

    Initial Commit

---
+++ Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4
+//
+//  Created by Nekosama on 4/12/18.
+//  Copyright © 2018 Nekosama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
